b50ff2df87569ac21f8c0f87cb0d6a122c2b0ed8
arina <arinakamilia21@gmail.com>
2021-01-25 21:44:18 +0700

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hitungbop;
-
-/**
- *
- * @author Putri Arina Kamilia
- */
-public class Hello {
-    
+class Hello{
+public static void main(String[] args){
+System.out.println("Arina: Aplikasi Java 2020");
+System.out.println("--- *** ---");
+}
 }
