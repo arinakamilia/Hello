4b084eb5f12a258fbcd506c18b21f722dd87d50f
arina <arinakamilia21@gmail.com>
2021-01-26 13:40:56 +0700

    Forth Commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("Arina: Aplikasi Java 2020");
 System.out.println("--- *** ---");
   System.out.println("END");
+	System.out.println("User 2");
 }
 }
